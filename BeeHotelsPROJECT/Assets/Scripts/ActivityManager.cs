93d5eca9ceb2f887e4c45d9406a3bac97a52cbe0
giovilasa <giovilasa@gmail.com>
2024-01-15 12:29:42 +0100

    Correzioni Attività
    Fixed Polline e Materiale

--- BeeHotelsPROJECT/Assets/Scripts/ActivityManager.cs
+++ BeeHotelsPROJECT/Assets/Scripts/ActivityManager.cs
@@ -5,63 +5,222 @@ using UnityEngine.UI;
 
 public class ActivityManager : MonoBehaviour
 {
-    public GameObject objectToActivate1;
-    public GameObject objectToActivate2;
-    public GameObject objectToActivate3;
-    public GameObject objectToActivate4;
-    public GameObject objectToActivate5;
-    public GameObject objectToActivate6;
+    public GameObject hotelA;
+    public GameObject hotelB;
+    public GameObject hotelC;
+    public GameObject hotelD;
 
-    public GameObject buttonToDeactivate;
-    
+    public GameObject objectToActivateA1;
+    public GameObject objectToActivateA2;
+    public GameObject objectToActivateA3;
+    public GameObject objectToActivateA4;
+    public GameObject objectToActivateA5;
+    public GameObject objectToActivateA6;
+
+    public GameObject objectToActivateB1;
+    public GameObject objectToActivateB2;
+    public GameObject objectToActivateB3;
+    public GameObject objectToActivateB4;
+    public GameObject objectToActivateB5;
+    public GameObject objectToActivateB6;
+
+    public GameObject objectToActivateC1;
+    public GameObject objectToActivateC2;
+    public GameObject objectToActivateC3;
+    public GameObject objectToActivateC4;
+    public GameObject objectToActivateC5;
+    public GameObject objectToActivateC6;
+
+    public GameObject objectToActivateD1;
+    public GameObject objectToActivateD2;
+    public GameObject objectToActivateD3;
+    public GameObject objectToActivateD4;
+    public GameObject objectToActivateD5;
+    public GameObject objectToActivateD6;
+
+    public GameObject buttonToDeactivateA;
+    public GameObject buttonToDeactivateB;
+    public GameObject buttonToDeactivateC;
+    public GameObject buttonToDeactivateD;
+
+    public GameObject cardToDeactivate4;
+    public GameObject cardToDeactivate5;
+    public GameObject newNidificazione;
+
     public GameObject windowCanvas;
-
     public AudioSource buttonClickAudioSource;
+
 
     public void OnButton1Click()
     {
-        objectToActivate1.SetActive(true);
-        buttonToDeactivate.SetActive(false);
+        if (hotelA.activeSelf)
+        {
+            objectToActivateA1.SetActive(true);
+            buttonToDeactivateA.SetActive(false);
+        }
+        else if (hotelB.activeSelf)
+        {
+            objectToActivateB1.SetActive(true);
+            buttonToDeactivateB.SetActive(false);
+        }
+        else if (hotelC.activeSelf)
+        {
+            objectToActivateC1.SetActive(true);
+            buttonToDeactivateC.SetActive(false);
+        }
+        else if (hotelD.activeSelf)
+        {
+            objectToActivateD1.SetActive(true);
+            buttonToDeactivateD.SetActive(false);
+        }
+
         windowCanvas.SetActive(false);
         buttonClickAudioSource.Play();
     }
 
     public void OnButton2Click()
     {
-        objectToActivate2.SetActive(true);
-        buttonToDeactivate.SetActive(false);
+        if (hotelA.activeSelf)
+        {
+            objectToActivateA2.SetActive(true);
+            buttonToDeactivateA.SetActive(false);
+        }
+        else if (hotelB.activeSelf)
+        {
+            objectToActivateB2.SetActive(true);
+            buttonToDeactivateB.SetActive(false);
+        }
+        else if (hotelC.activeSelf)
+        {
+            objectToActivateC2.SetActive(true);
+            buttonToDeactivateC.SetActive(false);
+        }
+        else if (hotelD.activeSelf)
+        {
+            objectToActivateD2.SetActive(true);
+            buttonToDeactivateD.SetActive(false);
+        }
+
         windowCanvas.SetActive(false);
         buttonClickAudioSource.Play();
     }
 
     public void OnButton3Click()
     {
-        objectToActivate3.SetActive(true);
-        buttonToDeactivate.SetActive(false);
+        if (hotelA.activeSelf)
+        {
+            objectToActivateA3.SetActive(true);
+            buttonToDeactivateA.SetActive(false);
+        }
+        else if (hotelB.activeSelf)
+        {
+            objectToActivateB3.SetActive(true);
+            buttonToDeactivateB.SetActive(false);
+        }
+        else if (hotelC.activeSelf)
+        {
+            objectToActivateC3.SetActive(true);
+            buttonToDeactivateC.SetActive(false);
+        }
+        else if (hotelD.activeSelf)
+        {
+            objectToActivateD3.SetActive(true);
+            buttonToDeactivateD.SetActive(false);
+        }
+
         windowCanvas.SetActive(false);
         buttonClickAudioSource.Play();
     }
 
     public void OnButton4Click()
     {
-        objectToActivate4.SetActive(true);
-        buttonToDeactivate.SetActive(false);
+        if (hotelA.activeSelf)
+        {
+            objectToActivateA4.SetActive(true);
+            buttonToDeactivateA.SetActive(false);
+            cardToDeactivate4.SetActive(false);
+        }
+        else if (hotelB.activeSelf)
+        {
+            objectToActivateB4.SetActive(true);
+            buttonToDeactivateB.SetActive(false);
+            cardToDeactivate4.SetActive(false);
+        }
+        else if (hotelC.activeSelf)
+        {
+            objectToActivateC4.SetActive(true);
+            buttonToDeactivateC.SetActive(false);
+            cardToDeactivate4.SetActive(false);
+        }
+        else if (hotelD.activeSelf)
+        {
+            objectToActivateD4.SetActive(true);
+            buttonToDeactivateD.SetActive(false);
+            cardToDeactivate4.SetActive(false);
+        }
+
         windowCanvas.SetActive(false);
         buttonClickAudioSource.Play();
     }
 
     public void OnButton5Click()
     {
-        objectToActivate5.SetActive(true);
-        buttonToDeactivate.SetActive(false);
+        if (hotelA.activeSelf)
+        {
+            objectToActivateA5.SetActive(true);
+            buttonToDeactivateA.SetActive(false);
+            cardToDeactivate5.SetActive(false);
+            newNidificazione.SetActive(true);
+        }
+        else if (hotelB.activeSelf)
+        {
+            objectToActivateB5.SetActive(true);
+            buttonToDeactivateB.SetActive(false);
+            cardToDeactivate5.SetActive(false);
+            newNidificazione.SetActive(true);
+        }
+        else if (hotelC.activeSelf)
+        {
+            objectToActivateC5.SetActive(true);
+            buttonToDeactivateC.SetActive(false);
+            cardToDeactivate5.SetActive(false);
+            newNidificazione.SetActive(true);
+        }
+        else if (hotelD.activeSelf)
+        {
+            objectToActivateD5.SetActive(true);
+            buttonToDeactivateD.SetActive(false);
+            cardToDeactivate5.SetActive(false);
+            newNidificazione.SetActive(true);
+        }
+
         windowCanvas.SetActive(false);
         buttonClickAudioSource.Play();
     }
 
     public void OnButton6Click()
     {
-        objectToActivate6.SetActive(true);
-        buttonToDeactivate.SetActive(false);
+        if (hotelA.activeSelf)
+        {
+            objectToActivateA6.SetActive(true);
+            buttonToDeactivateA.SetActive(false);
+        }
+        else if (hotelB.activeSelf)
+        {
+            objectToActivateB6.SetActive(true);
+            buttonToDeactivateB.SetActive(false);
+        }
+        else if (hotelC.activeSelf)
+        {
+            objectToActivateC6.SetActive(true);
+            buttonToDeactivateC.SetActive(false);
+        }
+        else if (hotelD.activeSelf)
+        {
+            objectToActivateD6.SetActive(true);
+            buttonToDeactivateD.SetActive(false);
+        }
+
         windowCanvas.SetActive(false);
         buttonClickAudioSource.Play();
     }
--- BeeHotelsPROJECT/Assets/Scripts/ScoreManager.cs
+++ BeeHotelsPROJECT/Assets/Scripts/ScoreManager.cs
@@ -8,11 +8,14 @@ public class ScoreManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText; // Riferimento all'oggetto Text per visualizzare il punteggio
 
+    private int baseScore = 5;
+    private int additionalScorePerActivityPress = 5;
+    private int activityPressCount = 0;
     private int score = 0;
-    private bool activityButtonPressed = false;
 
     public Button day3Button; // Riferimento al pulsante "Day3"
     public Button activityButton; // Riferimento al pulsante "Activity"
+
 
     private void Start()
     {
@@ -21,28 +24,15 @@ public class ScoreManager : MonoBehaviour
         UpdateScoreText();
     }
 
-    public void IncrementScore( )
+    public void Day3ButtonPressed()
     {
-        if (activityButtonPressed)
-        {
-            score += 10;
-        }
-        else
-        {
-            score += 5;
-        }
-
+        score += baseScore + (activityPressCount * additionalScorePerActivityPress);
         UpdateScoreText();
     }
 
     public void ActivityButtonPressed()
     {
-        activityButtonPressed = true;
-    }
-
-    public void Day3ButtonPressed()
-    {
-        IncrementScore();
+        activityPressCount++;
     }
 
     public void UpdateScoreText()
